f6eeb066c1b717d7bc633b41f236033fe091dde2
TGCAR <don.meln2013@yandex.ru>
2024-12-13 14:44:53 +0300

    BasicCourseWork1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class Main {
+
+    private final static Random RANDOM = new Random();
+    private final static String[] NAMES = {"Иван", "Петр", "Андрей", "Михаил", "Николай", "Александр", "Юрий", "Евгений", "Глеб", "Денис"};
+
+
+    private final static Employee[] EMPLOYEES = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
     }
 }
